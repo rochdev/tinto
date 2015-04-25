a6f5c9a980a8de19f5e94d5e445ea567a6227507
rochdev <roch.devost@gmail.com>
2015-04-25 11:00:48 -0400

    added return value with defined property names

--- test/utils/getters.spec.js
+++ test/utils/getters.spec.js
@@ -5,20 +5,31 @@ var expect = require('chai').use(sinonChai).expect;
 var getters = require('../../lib/utils/getters');
 
 describe('getters', function() {
-  it('should assign properties from source to destination', function() {
-    var source = {
+  var source;
+  var destination;
+
+  beforeEach(function() {
+    source = {
       get test() {
         return this.hello;
       }
     };
 
-    var destination = {
+    destination = {
       hello: 'world'
     };
+  });
 
+  it('should assign properties from source to destination', function() {
     getters(destination, source);
 
     expect(destination.test).to.be.defined;
     expect(destination.test).to.equal('world');
+  });
+
+  it('should return the newly defined properties', function() {
+    var props = getters(destination, source);
+
+    expect(props).to.deep.equal(['test']);
   });
 });
