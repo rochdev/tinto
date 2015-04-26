a72aae76ae3672c7315be1bdc57c6d4f98d4ddcb
rochdev <roch.devost@gmail.com>
2015-04-26 14:42:04 -0400

    updated to compare equality by value instead of by reference

--- test/attribute.spec.js
+++ test/attribute.spec.js
@@ -1,50 +1,87 @@
 'use strict';
 
-var rewire = require('rewire');
-//var sinon = require('sinon');
-var sinonChai = require('sinon-chai');
 var Q = require('q');
-var Attribute = rewire('../lib/attribute');
-var expect = require('chai').use(sinonChai).expect;
+var Attribute = require('../lib/attribute');
+var expect = require('chai').expect;
 
 describe('Attribute', function() {
   var promise;
   var value;
   var attribute;
 
-  beforeEach(function() {
-    value = 'test';
+  describe('given a primitive value"', function() {
+    beforeEach(function() {
+      value = 'test';
 
-    promise = Q.resolve(value);
+      promise = Q.resolve(value);
 
-    attribute = new Attribute(promise);
-  });
+      attribute = new Attribute(promise);
+    });
 
-  it('should assert to true for equality when actual and expected values match', function() {
-    return attribute.equals('test')().then(function(result) {
-      expect(result.outcome).to.be.true;
-      expect(result.actual).to.equal('test');
+    it('should assert to true for equality when actual and expected values match', function() {
+      return attribute.equals('test')().then(function(result) {
+        expect(result.outcome).to.be.true;
+        expect(result.actual).to.equal(value);
+      });
+    });
+
+    it('should assert to false for equality when actual and expected values differ', function() {
+      return attribute.equals('foo')().then(function(result) {
+        expect(result.outcome).to.be.false;
+        expect(result.actual).to.equal(value);
+      });
+    });
+
+    it('should assert to true for containing when actual contains expected', function() {
+      return attribute.contains('es')().then(function(result) {
+        expect(result.outcome).to.be.true;
+        expect(result.actual).to.equal(value);
+      });
+    });
+
+    it('should assert to false for containing when actual does not contain expected', function() {
+      return attribute.contains('foo')().then(function(result) {
+        expect(result.outcome).to.be.false;
+        expect(result.actual).to.equal(value);
+      });
     });
   });
 
-  it('should assert to false for equality when actual and expected values differ', function() {
-    return attribute.equals('foo')().then(function(result) {
-      expect(result.outcome).to.be.false;
-      expect(result.actual).to.equal('test');
-    });
-  });
+  describe('given an array', function() {
+    beforeEach(function() {
+      value = ['foo', 'bar'];
 
-  it('should assert to true for containing when actual contains expected', function() {
-    return attribute.contains('es')().then(function(result) {
-      expect(result.outcome).to.be.true;
-      expect(result.actual).to.equal('test');
-    });
-  });
+      promise = Q.resolve(value);
 
-  it('should assert to false for containing when actual does not contain expected', function() {
-    return attribute.contains('foo')().then(function(result) {
-      expect(result.outcome).to.be.false;
-      expect(result.actual).to.equal('test');
+      attribute = new Attribute(promise);
+    });
+
+    it('should assert to true for equality when actual and expected values match', function() {
+      return attribute.equals(['foo', 'bar'])().then(function(result) {
+        expect(result.outcome).to.be.true;
+        expect(result.actual).to.equal(value);
+      });
+    });
+
+    it('should assert to false for equality when actual and expected values differ', function() {
+      return attribute.equals(['baz'])().then(function(result) {
+        expect(result.outcome).to.be.false;
+        expect(result.actual).to.equal(value);
+      });
+    });
+
+    it('should assert to true for containing when actual contains expected', function() {
+      return attribute.contains('foo')().then(function(result) {
+        expect(result.outcome).to.be.true;
+        expect(result.actual).to.equal(value);
+      });
+    });
+
+    it('should assert to false for containing when actual does not contain expected', function() {
+      return attribute.contains('baz')().then(function(result) {
+        expect(result.outcome).to.be.false;
+        expect(result.actual).to.equal(value);
+      });
     });
   });
 });
