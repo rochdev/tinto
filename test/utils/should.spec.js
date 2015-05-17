23c91bda2916809f75d3fc5fc134b1e9129d572c
rochdev <roch.devost@gmail.com>
2015-05-17 10:17:46 -0400

    refactored to use mixin

--- test/utils/should.spec.js
+++ test/utils/should.spec.js
@@ -7,7 +7,7 @@ var expect = require('chai').use(sinonChai).expect;
 var should = rewire('../../lib/utils/should');
 
 describe('should', function() {
-  var descriptors;
+  var mixin;
   var context;
   var chai;
   var flag;
@@ -23,33 +23,21 @@ describe('should', function() {
       this.__flags = 'flags';
     });
 
-    descriptors = sinon.stub();
-    descriptors.withArgs(chai.Assertion.prototype).returns({
-      foo: {
-        value: 'bar'
-      },
-      hello: {
-        value: 'world'
-      }
-    });
+    mixin = sinon.spy();
 
     should.__set__('chai', chai);
     should.__set__('flag', flag);
-    should.__set__('descriptors', descriptors);
+    should.__set__('mixin', mixin);
   });
 
   it('should contain registered assertions', function() {
-    expect(should()).to.have.property('foo', 'bar');
+    var accessor = should();
+
+    expect(mixin).to.have.been.calledWith(accessor, chai.Assertion.prototype);
   });
 
   it('should contain assertion flags', function() {
     expect(should()).to.have.property('__flags', 'flags');
-  });
-
-  it('should not overwrite its own values when asserting', function() {
-    Function.prototype.hello = 'test';
-
-    expect(should()).to.have.property('hello', 'test');
   });
 
   it('should assert multiple delegated assertions', function() {
@@ -66,17 +54,13 @@ describe('should', function() {
 
   describe('eventually', function() {
     it('should contain registered assertions', function() {
-      expect(should().eventually).to.have.property('foo', 'bar');
+      var accessor = should().eventually;
+
+      expect(mixin).to.have.been.calledWith(accessor, chai.Assertion.prototype);
     });
 
     it('should contain assertion flags', function() {
       expect(should().eventually).to.have.property('__flags', 'flags');
-    });
-
-    it('should not overwrite its own values when asserting', function() {
-      Function.prototype.hello = 'test';
-
-      expect(should().eventually).to.have.property('hello', 'test');
     });
 
     it('should assert multiple delegated assertions', function() {
