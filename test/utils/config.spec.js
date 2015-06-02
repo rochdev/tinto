4bd1e1d719fb24ddd534d9d0b9ed7ff38f1523a0
rochdev <roch.devost@gmail.com>
2015-06-02 18:25:02 -0400

    added support for loading bundles by name

--- test/utils/config.spec.js
+++ test/utils/config.spec.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// TODO: support loading bundles by string
-
 var path = require('path');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
@@ -10,19 +8,24 @@ var expect = require('chai').use(sinonChai).expect;
 
 describe('config', function() {
   var config;
+  var fs;
   var bundle;
 
   beforeEach(function() {
     mockery.enable({
       useCleanCache: true,
-      warnOnUnregistered: false
+      warnOnUnregistered: false,
+      warnOnReplace: false
     });
+
+    fs = {
+      statSync: sinon.stub()
+    };
 
     bundle = sinon.stub();
 
+    mockery.registerMock('fs', fs);
     mockery.registerMock('./bundle', bundle);
-
-    config = require('../../lib/utils/config');
   });
 
   afterEach(function() {
@@ -30,59 +33,94 @@ describe('config', function() {
     mockery.disable();
   });
 
-  it('should return the right config for the key', function() {
-    mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
-      foo: 'bar'
+  describe('given a config file', function() {
+    beforeEach(function() {
+      config = require('../../lib/utils/config');
     });
 
-    var foo = config.get('foo');
+    it('should return the right config for the key', function() {
+      mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
+        foo: 'bar'
+      });
 
-    expect(foo).to.equal('bar');
+      var foo = config.get('foo');
+
+      expect(foo).to.equal('bar');
+    });
+
+    it('should use existing config if already set', function() {
+      mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
+        foo: 'bar'
+      });
+
+      config.get('foo');
+
+      mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
+        foo: 'baz'
+      });
+
+      var foo = config.get('foo');
+
+      expect(foo).to.equal('bar');
+    });
+
+    it('should load bundles by object', function() {
+      var foo = {};
+
+      mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
+        bundles: {
+          foo: foo
+        }
+      });
+
+      config.load();
+
+      expect(bundle).to.have.been.calledWith('foo', foo);
+    });
+
+    it('should load bundles by string', function() {
+      var foo = {};
+
+      mockery.registerMock(path.join(process.cwd(), 'node_modules', 'foo'), foo);
+      mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
+        bundles: {
+          foo: 'foo'
+        }
+      });
+
+      config.load();
+
+      expect(bundle).to.have.been.calledWith('foo', foo);
+    });
   });
 
-  it('should search up the directory tree when config file cannot be found', function() {
-    mockery.registerMock(path.join(path.resolve(process.cwd(), '..'), 'tinto.conf.js'), {
-      foo: 'baz'
+  describe('given no config file in the current folder', function() {
+    beforeEach(function() {
+      fs.statSync.onCall(0).throws(new Error('test'));
+      config = require('../../lib/utils/config');
     });
 
-    var foo = config.get('foo');
+    it('should search up the directory tree', function() {
+      mockery.registerMock(path.join(path.resolve(process.cwd(), '..'), 'tinto.conf.js'), {
+        foo: 'baz'
+      });
 
-    expect(foo).to.equal('baz');
+      var foo = config.get('foo');
+
+      expect(foo).to.equal('baz');
+    });
   });
 
-  it('should return the defaults if the file cannot be found at all', function() {
-    var bundles = config.get('bundles');
-
-    expect(bundles).to.deep.equal({});
-  });
-
-  it('should use existing config if already set', function() {
-    mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
-      foo: 'bar'
+  describe('given no config file exists', function() {
+    beforeEach(function() {
+      fs.statSync.throws(new Error('test'));
+      config = require('../../lib/utils/config');
     });
 
-    config.get('foo');
+    it('should return the defaults', function() {
+      var bundles = config.get('bundles');
 
-    mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
-      foo: 'baz'
+      expect(bundles).to.deep.equal({});
     });
-
-    var foo = config.get('foo');
-
-    expect(foo).to.equal('bar');
-  });
-
-  it('should load bundles', function() {
-    var foo = {};
-
-    mockery.registerMock(path.join(process.cwd(), 'tinto.conf.js'), {
-      bundles: {
-        foo: foo
-      }
-    });
-
-    config.load();
-
-    expect(bundle).to.have.been.calledWith('foo', foo);
   });
 });
